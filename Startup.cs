cde14ec227a831d207684afd170a3ec128d884cc
Wesley Sousa <wesleyyps@gmail.com>
2020-11-06 08:35:29 -0300

    master  - correct port for api to work without cert problems

--- Startup.cs
+++ Startup.cs
@@ -53,7 +53,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
